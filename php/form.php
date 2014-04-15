4dad9691c6314604a3165eea7466749481004113
Jesse Miller <jmiller@jmiller.com>
2014-04-14 19:11:26 -0500

    Form updates.

--- php/form.php
+++ php/form.php
@@ -3,9 +3,14 @@
 require_once('include/sqAES.php');
 require_once('include/javascrypt.php');
 
+Header('Content-type: text/plain');
+
+echo "What the browser POSTED:\n";
+print_r($_POST);
+
 javascrypt::decryptform();
 
-Header('Content-type: text/plain');
+echo "Unencrypted POST:\n";
 print_r($_POST);
 
 ?>
